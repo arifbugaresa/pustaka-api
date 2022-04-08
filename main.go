3b11823c4d876e8cca34c75e1de3f95b496e5b31
arif yuniarto <arif.yuniarto@nexsoft.co.id>
2022-04-08 13:52:51 +0700

    Config db heroku

--- main.go
+++ main.go
@@ -13,7 +13,9 @@ import (
 func main() {
 
 	// DB Connection
-	dsn := "host=localhost user=postgres password=paramadaksa dbname=pustaka port=5432 sslmode=require TimeZone=Asia/Jakarta"
+	//dsn := "host=localhost user=postgres password=paramadaksa dbname=pustaka port=5432 sslmode=require TimeZone=Asia/Jakarta"
+	dsn := "host=ec2-3-217-251-77.compute-1.amazonaws.com user=hbweazruihhypg password=2ba20672d00c6aaf213ddc22e5fe6c1a3f06d72920cf250872dbeca0ccbd9911 dbname=d8ut4jqc4ec0aa port=5432 sslmode=require TimeZone=Asia/Jakarta"
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalln("DB Connection Failed")
